af04761c1c25cb08a4b7b09e2d35346b227ea53f
zhaojun <>
2026-06-02 17:47:04 +0800

    更新license

--- site/src/config.ts
+++ site/src/config.ts
@@ -1,7 +1,7 @@
 export const APP_VERSION = '1.0.1'
 export const APP_NAME = 'AI Profile Manager'
 
-export const GITHUB_RELEASES = 'https://github.com/user/ai-profile-manager/releases'
+export const GITHUB_RELEASES = 'https://github.com/zhaojun2066/ai-profile-manager/releases'
 
 // All download links point to GitHub Releases
 export const DOWNLOAD_URL_ARM = GITHUB_RELEASES
